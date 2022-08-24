855fb050a5525a988b18c91d0401940eba5bddfa
reebhu12 <reebhugoku@gmail.com>
2022-08-24 18:05:01 +0530

    UNderstanding LIS properly and solving knapsack

--- src/SolutionLIS.java
+++ src/SolutionLIS.java
@@ -3,39 +3,35 @@ import java.util.Arrays;
 //Longest increasing subsequence
 public class SolutionLIS {
 
-    public static int findLIS(int [] arr, int len)
-    {
-        int lis[]= new int[len];
+    public static int findLIS(int[] arr, int index) {
+        int lis[] = new int[arr.length];
         Arrays.fill(lis,1);
-
-        for(int i=0;i<len;i++)
+        for(int i=0;i< arr.length;i++)
         {
-            int comp = arr[i];
             for(int j=0;j<i;j++)
             {
-                if(comp>arr[j] && lis[i]<lis[j]+1)
+                if(arr[i]>arr[j] && lis[i]<lis[j]+1) // Check the earlier computed value to find if the max is present before or not.
                 {
-                        lis[i]+=1;
+                    lis[i]+=1;
                 }
             }
         }
         return max(lis);
     }
 
-    static int  max (int... values )
-    {
+    static int max(int... values) {
         int maxi = values[0];
-        for(int i : values)
-        {
-            if(maxi<i)
+        for (int i : values) {
+            if (maxi < i)
                 maxi = i;
         }
         return maxi;
     }
 
     public static void main(String[] args) {
-        int arr[] =  { 0,1,0,3,2,3 };
-        System.out.println(findLIS(arr, arr.length));
+        int arr[] = {0, 1, 0, 3, 2, 3};
+        //int arr [] = {10,9,2,5,3,7,101,18};
+        System.out.println(findLIS(arr, arr.length - 1));
     }
 }
 
--- src/ZeroOneKnapsackProblem.java
+++ src/ZeroOneKnapsackProblem.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class ZeroOneKnapsackProblem {
+import java.util.Arrays;
+
+//Recursive with memoization
+public class ZeroOneKnapsackProblem {
+    static int[] memo;
+
+    public static void main(String[] args) {
+        int[] profit = {60, 100, 120};
+        int[] weight = {10, 20, 30};
+        int totalWeight = 50;
+        memo = new int[weight.length];
+        Arrays.fill(memo, -1);
+        int value = findProfit(weight, profit, totalWeight, weight.length - 1);
+        System.out.println(value);
+    }
+
+    private static int findProfit(int[] weight, int[] profit, int totalWeight, int index) {
+        if (totalWeight == 0 || index == 0) {
+            return 0;
+        }
+        if (weight[index] > totalWeight) {
+            return findProfit(weight, profit, totalWeight, index - 1);
+        }
+        int value;
+        if (memo[index - 1] != -1) {
+            value = memo[index-1];
+        } else {
+            value = findProfit(weight, profit, totalWeight, index - 1);
+            insertIntoMemo(value,index-1);
+        }
+        return Math.max(value + profit[index],
+                value);
+    }
+
+    private static void insertIntoMemo(int value, int i) {
+        memo[i]=value;
+    }
 }
